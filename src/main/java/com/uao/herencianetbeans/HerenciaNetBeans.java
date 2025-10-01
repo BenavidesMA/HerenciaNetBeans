44a384b90234ac371b73aad7a0a89c1a5d247b82
Miguel <miguelbv2302@gmail.com>
2025-10-01 10:37:45 -0500

    Se escribió una nueva linea de codigo

--- src/main/java/com/uao/herencianetbeans/HerenciaNetBeans.java
+++ src/main/java/com/uao/herencianetbeans/HerenciaNetBeans.java
@@ -12,5 +12,6 @@ public class HerenciaNetBeans {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Se hizo un cambio");
     }
 }
