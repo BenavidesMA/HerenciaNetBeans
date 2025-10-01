549e0415e7cf8b73b0281553a5f6a595d5e93759
Miguel <miguelbv2302@gmail.com>
2025-10-01 10:23:41 -0500

    Este es un nuevo commit

---
+++ src/main/java/com/uao/herencianetbeans/HerenciaNetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.herencianetbeans;
+
+/**
+ *
+ * @author Miguel
+ */
+public class HerenciaNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
